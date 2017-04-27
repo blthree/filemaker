8f4b9ee6a301888ed13a1db25c5e96041fef27a6
blthree <blthree89@gmail.com>
2017-04-27 19:43:54 -0400

    finally able to get a query working correctly

--- connect_to_FM.py
+++ connect_to_FM.py
@@ -1,4 +1,5 @@
 import pyodbc
+import datetime
 conn_str = 'Driver=FileMaker ODBC;AllAsText=0;ApplicationUsingThreads=1;FetchChunkSize=100;' \
            'FileOpenCache=0;IntlSort=0;MaxTextlength=255;ServerAddress=127.0.0.1;' \
            'TranslationOption=0;UseRemoteConnection=1;'
@@ -7,17 +8,23 @@ cnxn = pyodbc.connect("DSN=ABRC-DNAOrdItems;UID=admin;PWD=")
 # create a cursor object
 cursor = cnxn.cursor()
 j = cursor.tables()
-orders = cursor.execute('''select ABRC_stock_number, num_stocks_perstocknum from DNAOrdItem where num_stocks_perstocknum > 1''')
-print(type(orders))
-sql = '''select ABRC_stock_number, num_stocks_perstocknum from DNAOrdItem where num_stocks_perstocknum > ?'''
-test = []
-for row in cursor.execute(sql, 1):
-    test.append(row)
-    print(row)
+print(j)
+#orders = cursor.execute('''select ABRC_stock_number, num_stocks_perstocknum, Person_tair_id from DNAOrdItem where num_stocks_perstocknum > 1''')
+sql = '''select ABRC_stock_number, order_id_tair, date_order_placed from DNAOrdItem where date_order_shipped > ?'''
+orders = cursor.execute('''select ABRC_stock_number, num_stocks_perstocknum, Person_tair_id from DNAOrdItem where date_order_shipped > ?''')
+#orders = cursor.execute(sql, datetime.date(2016,6,30)).fetchall()
 
-a = test.copy()
+while orders is not None:
+    orders = cursor.execute(sql, datetime.date(2016, 6, 30)).fetchall()
+    print(len(orders))
+    test = []
+    for row in orders:
+        test.append(row)
+        print(row)
 
-print(a)
+#a = test.copy()
+
+#print(a)
 
 
 
